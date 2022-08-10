45ac6a01cbb3f177e57a8f95585ecd95211e8365
Mike <slavik433@gmail.com>
2022-08-10 16:10:20 +0300

    second step done

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,17 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const anim = document
+    .querySelector(".collapsible__content")
+    .animate(
+        [{opacity: 1}, {opacity: 0}],
+        {duration: 1, fill: "both"}
+    );
+
+anim.pause();
+
+document.querySelector("button").addEventListener("click", () => {
+    if (anim.playState === "paused") {
+        anim.play();
+    } else {
+        anim.reverse();
+    }
+});
+
