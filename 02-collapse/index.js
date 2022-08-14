6fcf1014f1f0657829ab3c52237fe68527067129
KobraMik <slavik433@gmail.com>
2022-08-14 19:26:33 +0300

    added button change (show/hide)

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,17 +1,37 @@
+const showBtn = document.querySelector(".collapsible__action--visible");
+const hideBtn = document.querySelector(".collapsible__action--hidden");
+let buttonChange = false
+
 const anim = document
     .querySelector(".collapsible__content")
     .animate(
         [{opacity: 1}, {opacity: 0}],
-        {duration: 1, fill: "both"}
+        {duration: 500, fill: "both"}
     );
 
 anim.pause();
 
-document.querySelector("button").addEventListener("click", () => {
+document.querySelector("button").addEventListener("click", hideContent);
+
+function hideContent() {
     if (anim.playState === "paused") {
         anim.play();
     } else {
         anim.reverse();
     }
-});
+    buttonChangeHandler()
+}
 
+function buttonChangeHandler() {
+    if (buttonChange) {
+        hideBtn.style.display = "block";
+        showBtn.style.display = "none";
+        buttonChange = !buttonChange
+    } else {
+        hideBtn.style.display = "none";
+        showBtn.style.display = "block";
+        buttonChange = !buttonChange
+    }
+}
+
+buttonChangeHandler()
